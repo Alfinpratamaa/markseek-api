7cea0ee6cbab5edb6d87a61fbb98ab9b9baa842a
muhamad alfin pratama <muhamadalfinpratamaa@gmail.com>
2025-02-27 15:44:40 +0700

    Add Swagger support to the application and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { Elysia, file } from "elysia";
 import { PORT } from "./config/env";
 import { Application } from "./Application";
 import cors from "@elysiajs/cors";
+import swagger from "@elysiajs/swagger";
 
 const app = new Elysia()
   // .use(staticPlugin())
+  .use(swagger())
   .use(cors())
   .group("/api", (app) => app.use(Application))
   .get("/verify", verifyEmail)
